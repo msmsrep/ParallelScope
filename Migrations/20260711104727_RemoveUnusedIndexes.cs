219bb08b8302a5cc3905c7d8634c2c9c3779a9ea
msmsrep <example@gmail.com>
2026-07-11 19:59:25 +0900

    不要なインデックスを削除マイグレーション

---
+++ Migrations/20260711104727_RemoveUnusedIndexes.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParallelScope.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnusedIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FileSystemEntries_IsFolder_Name",
+                table: "FileSystemEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FileSystemEntries_Name",
+                table: "FileSystemEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FileSystemEntries_ParentPath_Name",
+                table: "FileSystemEntries");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_FileSystemEntries_IsFolder_Name",
+                table: "FileSystemEntries",
+                columns: new[] { "IsFolder", "Name" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileSystemEntries_Name",
+                table: "FileSystemEntries",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileSystemEntries_ParentPath_Name",
+                table: "FileSystemEntries",
+                columns: new[] { "ParentPath", "Name" });
+        }
+    }
+}
